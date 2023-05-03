ef6572a979c764caaf9296ca93c13a30af9bcfca
Michael Day <mday299@yahoo.com>
2023-05-03 18:39:13 -0400

    added C_sharp and NuGet folders

---
+++ NuGet/example1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace Nuget.Quickstart
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account
+            {
+                Name = "John Doe",
+                Email = "john@nuget.org",
+                DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc),
+            };
+
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
