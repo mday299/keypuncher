5211936675c95e9fbf5aadae51c1701d088c21a9
Michael Day <michael.day@gtri.gatech.edu>
2023-11-01 19:18:18 -0400

    testing socket

---
+++ C++/Networking/simpleSocketTestCopy/clientTCP.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+    // Create a socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::cerr << "Error creating socket" << std::endl;
+        return 1;
+    }
+
+    // Connect to the server
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(12345); // Port number
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP 
+
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Error connecting to server" << std::endl;
+        close(clientSocket);
+        return 2;
+    }
+
+    // Receive data from the server
+    char buffer[1024] = {0};
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    std::cout << "Server says: " << buffer << std::endl;
+
+    // Close socket
+    close(clientSocket); 
+
+    return 0;
+}
