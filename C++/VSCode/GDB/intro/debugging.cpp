8535c7c294f8394983df32f2ad1713adf86c47de
Michael Day <mday299@yahoo.com>
2024-04-15 18:26:57 -0400

    broke out loop into its own function.

--- C++/VSCode/GDB/intro/debugging.cpp
+++ C++/VSCode/GDB/intro/debugging.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
+int loopInt (int iterations) {
+    int i;
+    for (i = 0; i < iterations; i++) {
+        std::cout << i << std::endl;
+    }
+}
+
 int main (int argc, char *argv[]) { 
     std::cout << "Howdy" << std::endl;
 
-    int i; 
-    for (i = 0; i < 10; i++) {
-        std::cout << i << std::endl;
-    }
+    loopInt(5);
 
     return 0;
 }
