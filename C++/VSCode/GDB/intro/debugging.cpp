20229d13639daea85ba1f3ae0fe3b13121e598b3
Michael Day <mday299@yahoo.com>
2024-04-15 17:13:18 -0400

    preliminary gdb intro files

---
+++ C++/VSCode/GDB/intro/debugging.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main (int argc, char *argv[]) { 
+    std::cout << "Howdy" << std::endl;
+
+    int i; 
+    for (i = 0; i < 10; i++) {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
