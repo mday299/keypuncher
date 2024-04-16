bbeb404bd1f12fd75da4d1ad88be23dc663ad431
Michael Day <mday299@yahoo.com>
2024-04-16 16:26:27 -0400

    Added variables to debugging.cpp and got specific with launch.json

--- C++/VSCode/GDB/intro/debugging.cpp
+++ C++/VSCode/GDB/intro/debugging.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <string>
 
 int loopInt (int iterations) {
     int i;
     for (i = 0; i < iterations; i++) {
         std::cout << i << std::endl;
     }
+    return i;
 }
 
 int main (int argc, char *argv[]) { 
     std::cout << "Howdy" << std::endl;
 
-    loopInt(5);
+    float theFloat = 8;
+    std::string theString = "Boo!";
+    int it;
+
+    it = loopInt(5);
 
     return 0;
 }
