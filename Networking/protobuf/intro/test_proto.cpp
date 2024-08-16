b5ce1c55f4b3f72790ca9011295932873221ec49
Michael Day <mday299@yahoo.com>
2024-08-16 09:06:25 -0400

    Preliminary CMakeLists.txt file

--- Networking/protobuf/intro/test_proto.cpp
+++ Networking/protobuf/intro/test_proto.cpp
@@ -2,8 +2,19 @@
 
 #include "test.pb.h"
 
+void printPosition(const example::Position& position) {
+    std::cout << "Aircraft ID: " << position.aircraft_id() << std::endl;
+    std::cout << "Aircraft Name: " << position.aircraft_name() << std::endl;
+    std::cout << "Latitude: " << position.lat_deg() << std::endl;
+    std::cout << "Longitude: " << position.lon_deg() << std::endl;
+    std::cout << "Altitude: " << position.alt_msl() << std::endl;
+    std::cout << "Altitude Units: " << (position.alt_units() == example::Position::METERS ? "Meters" : "Feet") << std::endl;
+}
+
 int main(int argc, char* argv[]) {
     GOOGLE_PRTOBUF_VERIFY_VERSION;
 
     std::cout << "Howdy" << std:endl;
+
+    printPostition();
 }
