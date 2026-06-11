44f84e062e9d894d60965c6d144596bb7831581d
Michael Day <michael@keypuncher.net>
2026-06-11 10:40:06 -0400

    added string type

--- C++/Auto/auto.cpp
+++ C++/Auto/auto.cpp
@@ -1,10 +1,11 @@
 /*
 Write a program that automatically deduces the type for char, int,
- and double objects based on the initializer used. 
+ double and string objects based on the initializer used. 
 Print out the values afterward.
 Then determine the type programmatically 
  WITHOUT forcing a compile time error 
  (not always possible with more complex types)
+
 */
 
 #include <iostream>
@@ -14,12 +15,15 @@ int main() {
     auto character = 'f';
     auto integer = 2;
     auto dbl = 2.3;
+    auto str = "boogie";
 
-    std::cout << character << ", " << integer << ", " << dbl << std::endl;
+    std::cout << character << ", " << integer << ", " << dbl <<
+        ", " << str << std::endl;
     
     std::cout << typeid(character).name() << std::endl;
     std::cout << typeid(integer).name() << std::endl;
     std::cout << typeid(dbl).name() << std::endl;
+    std::cout << typeid(str).name() << std::endl;  
 
     return 0;
 }
